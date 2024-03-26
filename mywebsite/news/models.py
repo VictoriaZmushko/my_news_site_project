26addde890af22018bfe82d6bc9e0466893166cd
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-26 21:15:12 +0300

    change meta class

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Reporter(models.Model):
-    full_name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.full_name
-    
 
 class Article(models.Model):
     publish = models.DateField(default=timezone.now)
     headline = models.CharField(max_length=200)
     content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(
+        User, on_delete = models.CASCADE, related_name = "news_articles"
+    )
 
     class Meta:
         ordering = ["-publish"]
