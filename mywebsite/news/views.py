38d780e486d6428ddabf769f37e749eaecc6b343
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-27 21:55:52 +0300

    building list and detail views

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+from .models import Article
+
+
+def article_list(request, id):
+    article = get_object_or_404(article, id=id)
+    return render(request, "news/article/list.html", {"article": article})
+
+
+def article_list(request):
+    articles = Article.published.all()
+    return render(request, "news/article/list.html", {"articles": articles})
