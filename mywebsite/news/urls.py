d9b896c42ea13fad1a25931d441aeb3295c96910
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-28 22:27:45 +0300

    deleting comments

--- mywebsite/news/urls.py
+++ mywebsite/news/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = "news"
 urlpatterns = [
-    # post
     path("", views.article_list, name="article_list"),
     path("<int:id>/", views.article_detail, name="article_detail"),
 ]
